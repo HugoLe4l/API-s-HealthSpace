aac9f1736d5c4b1ca0a8935149da2bf9d98151c5
Hugo <164691382+HugoLe4l@users.noreply.github.com>
2025-03-20 17:04:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ const PORT = 8081;
 
 app.use(express.json());
 const corsOptions = {
-    origin: '*', // Permite qualquer origem
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true, // Permite o envio de cookies/tokens, se necessário
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-  };
-  
-  app.use(cors(corsOptions)); // Aplica as configurações do CORS
+  origin: ['http://localhost:5173', 'https://api-s-health-space.vercel.app'], // Permite a origem do frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Para enviar cookies/tokens, se necessário
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+};
 
+app.use(cors(corsOptions)); // Aplica as configurações de CORS
 
 app.use('/auth', AuthRota); // Rotas de autenticação
 app.use('/anuncio', AnuncioRota); // Rotas de autenticação
